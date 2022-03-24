754bef5fcbb18b3f2b8d33231fbb03b61a2898d5
Christian Brown <robrowno@icloud.com>
2022-03-24 20:44:21 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,8 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -72,6 +74,8 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented Operator ${operator}`);
         throw(`Unimplemented Operator ${operator}. Aborting!`);
@@ -114,7 +118,10 @@ function displaySubstractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
 
 }
 
